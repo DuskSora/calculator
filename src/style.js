3b2feecb74cd0f1a351becf3bf52d0fb472c6da7
DuskSora <DuskSora@Gmail.com>
2018-11-15 17:46:40 +0800

    calculator

---
+++ src/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const CalcWrapper = styled.div`
+    position: relative;
+    display: block;
+    width: 400px;
+    height: 600px;
+    background: red;
+`;
+
+export const Screen = styled.div`
+    position: absolute;
+    width: 380px;
+    height: 100px;
+    background: green;
+    margin: 0 auto;
+`;
